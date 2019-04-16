26cbe916f7c2f7caa9fd1dd584f4eaa3499bfaac
Reina Murooka <reinamurooka@gmail.com>
2019-04-16 16:36:11 -0400

    f: Login Form & Home Page must route links to proper components

--- client/components/auth-form.js
+++ client/components/auth-form.js
@@ -2,6 +2,8 @@ import React from 'react'
 import {connect} from 'react-redux'
 import PropTypes from 'prop-types'
 import {auth} from '../store'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
 
 /**
  * COMPONENT
@@ -11,7 +13,31 @@ const AuthForm = props => {
 
   return (
     <div>
-      <form onSubmit={handleSubmit} name={name}>
+      <Form onSubmit={handleSubmit} name={name}>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" name="email" />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            name="password"
+          />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          {displayName}
+        </Button>
+        {error && error.response && <div> {error.response.data} </div>}
+      </Form>
+      <a href="/auth/google">{displayName} with Google</a>
+    </div>
+    /* {<form onSubmit={handleSubmit} name={name}>
         <div>
           <label htmlFor="email">
             <small>Email</small>
@@ -25,12 +51,12 @@ const AuthForm = props => {
           <input name="password" type="password" />
         </div>
         <div>
-          <button type="submit">{displayName}</button>
-        </div>
-        {error && error.response && <div> {error.response.data} </div>}
-      </form>
-      <a href="/auth/google">{displayName} with Google</a>
-    </div>
+          <button type="submit">{displayName}</button>} */
+    /* </div> */
+    // {error && error.response && <div> {error.response.data} </div>}
+    // </form>
+    // <a href="/auth/google">{displayName} with Google</a>
+    // </div>
   )
 }
 
