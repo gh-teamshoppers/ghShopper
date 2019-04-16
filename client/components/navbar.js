1fb11c5ee21f3287971f33666d6a40019d0bb829
KV Nguyen <kieuvyn12@gmail.com>
2019-04-16 11:59:22 -0400

    feature: adds navbar component

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -3,29 +3,52 @@ import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
 import {logout} from '../store'
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import NavDropdown from 'react-bootstrap/NavDropdown'
 
-const Navbar = ({handleClick, isLoggedIn}) => (
-  <div>
-    <h1>BOILERMAKER</h1>
-    <nav>
-      {isLoggedIn ? (
-        <div>
-          {/* The navbar will show these links after you log in */}
-          <Link to="/home">Home</Link>
-          <a href="#" onClick={handleClick}>
-            Logout
-          </a>
-        </div>
-      ) : (
-        <div>
-          {/* The navbar will show these links before you log in */}
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Sign Up</Link>
-        </div>
-      )}
-    </nav>
-    <hr />
-  </div>
+const Navbarfunc = ({handleClick, isLoggedIn}) => (
+  // <div>
+  //   <h1>BOILERMAKER</h1>
+  //   <nav>
+  //     {isLoggedIn ? (
+  //       <div>
+  //         {/* The navbar will show these links after you log in */}
+  //         <Link to="/home">Home</Link>
+  //         <a href="#" onClick={handleClick}>
+  //           Logout
+  //         </a>
+  //       </div>
+  //     ) : (
+  //       <div>
+  //         {/* The navbar will show these links before you log in */}
+  //         <Link to="/login">Login</Link>
+  //         <Link to="/signup">Sign Up</Link>
+  //       </div>
+  //     )}
+  //   </nav>
+  //   <hr />
+  // </div>
+
+  <Navbar bg="light" expand="lg">
+    <Navbar.Brand href="#home">Expresso.js</Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="mr-auto">
+        <Nav.Link href="#home">Home</Nav.Link>
+        <Nav.Link href="#link">About Us</Nav.Link>
+        <NavDropdown title="Our Coffee" id="basic-nav-dropdown">
+          <NavDropdown.Item href="#action/3.1">Light Roast</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.2">Medium Roast</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.3">Dark Roast</NavDropdown.Item>
+          <NavDropdown.Divider />
+          <NavDropdown.Item href="#action/3.4">All Coffee</NavDropdown.Item>
+        </NavDropdown>
+        <Nav.Link href="#login">Sign In</Nav.Link>
+        <Nav.Link href="#cart">Cart</Nav.Link>
+      </Nav>
+    </Navbar.Collapse>
+  </Navbar>
 )
 
 /**
@@ -45,7 +68,7 @@ const mapDispatch = dispatch => {
   }
 }
 
-export default connect(mapState, mapDispatch)(Navbar)
+export default connect(mapState, mapDispatch)(Navbarfunc)
 
 /**
  * PROP TYPES
