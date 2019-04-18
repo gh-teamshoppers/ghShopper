a319515a4d03d8a08d239dc74f58d86de3f17806
KV Nguyen <kieuvyn12@gmail.com>
2019-04-18 12:26:37 -0400

    feature: Single Coffee Page Component is hooked up to the All Coffees Component through the SIngle Coffee Component

--- client/components/SingleCoffeePage.js
+++ client/components/SingleCoffeePage.js
@@ -3,33 +3,45 @@ import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
 import Jumbotron from 'react-bootstrap/Jumbotron'
 import Button from 'react-bootstrap/Button'
+import {getSingleCoffee} from '../store'
 
-export default class SingleCoffeePage extends React.Component {
+export class SingleCoffeePage extends React.Component {
+  componentDidMount() {
+    this.props.getCoffee(this.props.match.params.coffeeId)
+  }
   render() {
-    const {name, description, price, origin, preparation} = this.props.coffee
-
+    const {
+      name,
+      description,
+      preparation,
+      origin,
+      weight,
+      price,
+      imgUrl
+    } = this.props.coffees.coffee
     return (
       <Jumbotron>
-        <h1>
-          Sample Coffee Name
-          {/* {name} */}
-        </h1>
-        <img src="https://cdn1.medicalnewstoday.com/content/images/articles/323/323594/white-cup-with-black-coffee.jpg" />
+        <h1>{name}</h1>
+        <img src={imgUrl} />
         <p>
-          Description goes here:
-          {/* {description} */}
-        </p>
-        <p>
-          Price:
-          {/* {price} */}
+          Description:
+          {description}
         </p>
         <p>
           Preparation:
-          {/* {preparation} */}
+          {preparation}
         </p>
         <p>
           Origin:
-          {/* {origin} */}
+          {origin}
+        </p>
+        <p>
+          Weight:
+          {weight}
+        </p>
+        <p>
+          Price:
+          {price}
         </p>
         <p>
           <Button variant="primary">Learn more!</Button>
@@ -38,3 +50,13 @@ export default class SingleCoffeePage extends React.Component {
     )
   }
 }
+
+const mapStateToProps = state => ({
+  coffees: state.coffees
+})
+
+const mapDispatchToProps = dispatch => ({
+  getCoffee: coffeeId => dispatch(getSingleCoffee(coffeeId))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(SingleCoffeePage)
