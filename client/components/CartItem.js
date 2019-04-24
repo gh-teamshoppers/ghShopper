87acee9d15c45908d21959023e49c3f80cfb04b0
Macarena <macarena.carreno@gmail.com>
2019-04-24 03:05:53 -0400

    render cart for userLoggedIn

--- client/components/CartItem.js
+++ client/components/CartItem.js
@@ -3,6 +3,7 @@ import Card from 'react-bootstrap/Card'
 
 const CartItem = props => {
   const {imgUrl, quantity, price, weight, name} = props.item
+  //const qty = props.item.OrdersProducts.quantity
   return (
     <Card style={{width: '600px'}} className="cart">
       <Card.Body>
